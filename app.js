b5425f997ec055c205cb4ce4adb8a8ca3b39ae2e
Martti Raavel <mrt@mrt.ee>
2024-02-26 17:22:16 +0200

    Kolmanda loengu kood

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('Hello World!');
+const prices = [1, 10, 3, 4, 5];
+
+let biggestNumber = 0;
+
+for (let i = 0; i < prices.length; i ++) {
+    if (prices[i] > biggestNumber) {
+        biggestNumber = prices[i];
+    }
+}
+
+console.log(biggestNumber);
