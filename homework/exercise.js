a616a21ab642613b3f12d46b0ebc5938644a59bf
mrtrvl <mrt@mrt.ee>
2024-03-03 07:21:04 +0200

    Create exercise.js

---
+++ homework/exercise.js
@@ -0,0 +1,5 @@
+const fruits = ['apple', 'banana', 'orange', 'mango', 'kiwi', 'peach', 'pear', 'plum', 'grape', 'cherry'];
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
