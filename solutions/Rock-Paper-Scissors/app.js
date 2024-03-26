da88fb416a2e212e8de99f729b78229c18a521b9
mrtrvl <mrt@mrt.ee>
2024-03-26 16:48:37 +0200

    Kivi-Paber-Käärid Variant 2

--- solutions/Rock-Paper-Scissors/app.js
+++ solutions/Rock-Paper-Scissors/app.js
@@ -3,15 +3,65 @@ const colors = require('colors');
 const messages = require('./messages');
 
 console.log(messages.gameMessage.bgGreen.black);
-console.log('-'.repeat(messages.playerOneChoice.length));
 
-const userOneChoice = prompt(messages.playerOneChoice, { echo: '*'});
-const userTwoChoice = prompt(messages.playerTwoChoice, { echo: '*'});
+function line(length) {
+  return '-'.repeat(length);
+}
 
-if (userOneChoice === userTwoChoice) {
-  console.log(messages.draw.green);
-} else if (userOneChoice === '1' && userTwoChoice === '3' || userOneChoice === '2' && userTwoChoice === '1' || userOneChoice === '3' && userTwoChoice === '2') {
+const availableChoices = ['1', '2', '3', 'q'];
+
+const points = {
+  playerOne: 0,
+  playerTwo: 0,
+};
+
+function displayPoints() {
+  console.log('------ Hetke seis -------');
+  console.log('| Mängija 1 | Mängija 2 |');
+  console.log('|' + line(23) + '|');
+  console.log('|     ' + points.playerOne + '     |     ' + points.playerTwo + '     |');
+  console.log(line(25));
+}
+
+function detectWinner(userOneChoice, userTwoChoice) {
+  if (userOneChoice === userTwoChoice) {
+    return messages.draw.green;
+  } else if (userOneChoice === '1' && userTwoChoice === '3' || userOneChoice === '2' && userTwoChoice === '1' || userOneChoice === '3' && userTwoChoice === '2') {
+    points.playerOne++;
+    return messages.playerOneWin.green;
+  } else {
+    points.playerTwo++;
+    return messages.playerTwoWin.green;
+  }
+}
+
+const pointsToWin = Number(prompt(messages.pointsToWin));
+
+while (points.playerOne < pointsToWin && points.playerTwo < pointsToWin) {
+  console.log(line(40));
+  const userOneChoice = prompt(messages.playerOneChoice, { echo: '*'});
+  const userTwoChoice = prompt(messages.playerTwoChoice, { echo: '*'});
+  console.log(line(40));
+
+  if (!availableChoices.includes(userOneChoice) || !availableChoices.includes(userTwoChoice)) {
+    console.log('Vigane valik!'.red);
+    continue;
+  }
+
+  if (userOneChoice === 'q' || userTwoChoice === 'q') {
+    console.log('Mäng lõpetatud!'.bgRed.white);
+    break;
+  }
+
+  console.log(detectWinner(userOneChoice, userTwoChoice));
+  displayPoints();
+}
+
+if (points.playerOne > points.playerTwo) {
   console.log(messages.playerOneWin.green);
+} else if (points.playerOne === points.playerTwo){
+  console.log(messages.draw.green);
 } else {
   console.log(messages.playerTwoWin.green);
 }
+
--- solutions/Rock-Paper-Scissors/messages.js
+++ solutions/Rock-Paper-Scissors/messages.js
@@ -1,10 +1,11 @@
 const messages = {
   gameMessage: 'Tere tulemast mängima kivi-paber-käärid mängu!',
   draw: 'Viik!',
-  playerOneChoice: 'Mängija 1, vali kas kivi - ' + ' 1 '.bgRed.white + ', paber - ' + ' 2 '.bgRed.white + ' või käärid - '+ ' 3 '.bgRed.white + ': ',
-  playerTwoChoice: 'Mängija 2, vali kas kivi - ' + ' 1 '.bgRed.white + ', paber - ' + ' 2 '.bgRed.white + ' või käärid - '+ ' 3 '.bgRed.white + ': ',
+  playerOneChoice: 'Mängija 1, vali kas kivi - ' + ' 1 '.bgRed.white + ', paber - ' + ' 2 '.bgRed.white + ' või käärid - '+ ' 3 '.bgRed.white + '. Lõpetamine - ' + ' q '.bgRed.white + ': ',
+  playerTwoChoice: 'Mängija 2, vali kas kivi - ' + ' 1 '.bgRed.white + ', paber - ' + ' 2 '.bgRed.white + ' või käärid - '+ ' 3 '.bgRed.white + '. Lõpetamine - ' + ' q '.bgRed.white + ': ',
   playerOneWin: 'Mängija 1 võitis!',
   playerTwoWin: 'Mängija 2 võitis!',
+  pointsToWin: 'Mitme punktini mäng käib? ',
 };
 
 module.exports = messages;
